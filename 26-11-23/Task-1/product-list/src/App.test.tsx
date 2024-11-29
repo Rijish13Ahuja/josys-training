82a5786e000e406ccdfc12e4e5e88ae188236f9c
Rijish Ahuja <rhythm00ahuja@gmail.com>
2024-11-29 10:46:47 +0530

    changes

--- 26-11-23/Task-1/product-list/src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders Products title", () => {
-  render(<App />);
-  const titleElement = screen.getByText(/Products/i);
-  expect(titleElement).toBeInTheDocument();
-});
